5a3f568c2a8f23c2b3f58a8f20fca46ce954e314
NJL7502L <g.hana1211.tako@gmail.com>
2020-06-23 20:40:23 +0900

    いろいろついか

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <Arduino.h>
+// Device
+const int supVoltage_mv = 3300;
 const int pinX = 12;
 const int pinY = 13;
+const int pinReference = 14;
+const int pwmResolution = 256;
+
+// Oscilloscope
+const int voltageScale_mv = 1000;
+const int gridCnt = 10;
+const int plotVoltageRange = voltageScale_mv * gridCnt;
+
+// 
+int referenceVoltage_mv = 0;
+int maximumVoltage_mv = 0;
+int minimumVoltage_mv = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  // お城の描写可能領域から最大電圧/リファレンス電圧を設定
+  if (supVoltage_mv > plotVoltageRange){
+    referenceVoltage_mv = plotVoltageRange / 2;
+    maximumVoltage_mv = plotVoltageRange;
+  }else{
+    referenceVoltage_mv = supVoltage_mv / 2;
+    maximumVoltage_mv = supVoltage_mv;
+  }
+
   pinMode(pinX,OUTPUT);
   pinMode(pinY,OUTPUT);
 }
