b08356da1946c3c5f7d04a048ae0a25b18430d93
NJL7502L <g.hana1211.tako@gmail.com>
2020-07-04 12:29:20 +0900

    オシロに便利な設定があったしめんどくさいことしすぎてたの で簡潔にする

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,18 @@
 #include "parameter.h"
 
 void setup() {
-  // お城の描写可能領域から最大電圧/リファレンス電圧を設定
-  if (supVoltage_mv > plotVoltageRange){
-    referenceVoltage_mv = plotVoltageRange / 2;
-    maximumVoltage_mv = plotVoltageRange;
-  }else{
-    referenceVoltage_mv = supVoltage_mv / 2;
-    maximumVoltage_mv = supVoltage_mv;
-  }
-
   pinMode(pinX,OUTPUT);
   pinMode(pinY,OUTPUT);
 }
 
 void plot(float x,float y){
-  float outX_mv = map(x*100,originX_min*100,originX_max*100,0,maximumVoltage_mv);
-  float outY_mv = map(y*100,originY_min*100,originY_max*100,0,maximumVoltage_mv);
-  int outX = map(outX_mv*100,0,maximumVoltage_mv*100,0,pwmResolution);
-  int outY = map(outY_mv*100,0,maximumVoltage_mv*100,0,pwmResolution);
-  int outRef = map(referenceVoltage_mv*100,0,maximumVoltage_mv*100,0,pwmResolution);
+  float outX = map(x*100,0,100,0,255);
+  float outY = map(y*100,0,100,0,255);
   analogWrite(pinX,outX);
   analogWrite(pinY,outY);
-  analogWrite(pinReference,outRef);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  plot(1,-1);
+  plot(0.2,0.2);
 }
