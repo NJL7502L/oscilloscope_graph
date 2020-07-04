7ca16159df1c5606674ae8e740283d47b7ae1501
NJL7502L <g.hana1211.tako@gmail.com>
2020-07-04 12:33:45 +0900

    正負の反転

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,13 @@ void setup() {
 }
 
 void plot(float x,float y){
-  float outX = map(x*100,0,100,0,255);
-  float outY = map(y*100,0,100,0,255);
+  float outX = map(x*100,100,0,0,255);
+  float outY = map(y*100,100,0,0,255);
   analogWrite(pinX,outX);
   analogWrite(pinY,outY);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  plot(0.2,0.2);
+  plot(0.8,0.8);
 }
